de5c6d4a957c0ead9a528ba2d2e7b4a82618396c
Lisi Tomelin <lisa_tomelin@hotmail.com>
2024-09-24 20:18:04 -0300

    add: javascript

---
+++ controleEstacionamento.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  const form = document.getElementById("formEstacionamento");
+  const tabela = document.querySelector("#tabelaEstacionamento tbody");  
+  
+  
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    
+    const placa = document.getElementById("placaVeiculo").value;
+    const nomeProprietario = document.getElementById("nomeProprietario").value;
+    const numeroApartamento = document.getElementById("numeroDoApartamento").value;
+    const bloco = document.getElementById("blocoDoApartamento").value;
+    const modelo = document.getElementById("modeloDoVeiculo").value;
+    const cor = document.getElementById("corDoVeiculo").value;
+    const numVaga = document.getElementById("NumVagaEstacionamento").value;
+    
+    const novaLinha = document.createElement("tr");
+    
+    novaLinha.innerHTML = `      
+      <td>${placa}</td>
+      <td>${nomeProprietario}</td>
+      <td>${numeroApartamento}</td>
+      <td>${bloco}</td>
+      <td>${modelo}</td>
+      <td>${cor}</td>
+      <td>${numVaga}</td>
+    `;
+    
+    tabela.appendChild(novaLinha);
+    
+    form.reset();
+  });
+});
