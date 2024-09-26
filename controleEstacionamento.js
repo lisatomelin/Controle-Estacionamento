f9ae94fe9fa103e1f686de4a8b28ef78051d3d02
Lisi Tomelin <lisa_tomelin@hotmail.com>
2024-09-26 19:58:17 -0300

    add: console.log

--- controleEstacionamento.js
+++ controleEstacionamento.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
- 
+
   const form = document.getElementById("formEstacionamento");
-  const tabela = document.querySelector("#tabelaEstacionamento tbody");  
-  
-  
+  const tabela = document.querySelector("#tabelaEstacionamento tbody");
+
+
   form.addEventListener("submit", function (event) {
-    event.preventDefault(); 
-    
+    event.preventDefault();
+
+
     const placa = document.getElementById("placaVeiculo").value;
     const nomeProprietario = document.getElementById("nomeProprietario").value;
     const numeroApartamento = document.getElementById("numeroDoApartamento").value;
@@ -14,9 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const modelo = document.getElementById("modeloDoVeiculo").value;
     const cor = document.getElementById("corDoVeiculo").value;
     const numVaga = document.getElementById("NumVagaEstacionamento").value;
-    
+
+    console.log("\nPlaca do Veículo:" + placa + ", " +
+      "\nNome do Proprietário:" + nomeProprietario + ", " +
+      "\nNúmero do Apartamento:" + numeroApartamento + ", " +
+      "\nNúmero da Vaga:" + numVaga + ", " +
+      "\nBloco:" + bloco + ", " +
+      "\nModelo do Veículo:" + modelo + ", " +
+      "\nCor do Veículo:" + cor + ", ")
+
     const novaLinha = document.createElement("tr");
-    
+
     novaLinha.innerHTML = `      
       <td>${placa}</td>
       <td>${nomeProprietario}</td>
@@ -26,9 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
       <td>${cor}</td>
       <td>${numVaga}</td>
     `;
-    
+
     tabela.appendChild(novaLinha);
-    
+
     form.reset();
   });
 });
